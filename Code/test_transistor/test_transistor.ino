00fcee12d7749b0440f6d5450dba46fb5bf06a0e
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-06-27 14:24:50 +0200

    Version 0.5.2
    The code now works correctly and beautifully.

--- Code/test_transistor/test_transistor.ino
+++ Code/test_transistor/test_transistor.ino
@@ -1,3 +1,5 @@
+int myPins[] = {14, 15, 16, 17, 18, 19, 20};
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,6 +7,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(20));
+  if(getChanged() != 0){
+    Serial.println(getChanged());
+  }
   delay(100);
 }
+
+int getChanged(){
+  int ret = 0;
+  for (int i = 0; i < sizeof(myPins); i++) {
+    if ((analogRead(myPins[i])) < 10) {
+      ret = myPins[i];
+    }
+  }
+  if(ret < 0){
+    ret = 0;
+  }else if(ret > 25){
+    ret = 0;
+  }
+  return ret;
+}
+
