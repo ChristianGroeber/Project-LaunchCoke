56b3a403bbeb95f83d1575636917b73187009dc0
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-06-28 14:02:08 +0200

    Version 3.1
    First working version of a Servo that just goes from one side to the other

---
+++ Code/test_servo/test_servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo myServo;
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(7);
+  Serial.begin(9600);
+}
+
+int initialPosition = 0;
+int toAdd;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(initialPosition == 180){
+    toAdd = -20;
+  }else if(initialPosition == 0){
+    toAdd = 20;
+  }
+  initialPosition = initialPosition + toAdd;
+  myServo.write(initialPosition);
+  Serial.println(initialPosition);
+  delay(500);
+}
